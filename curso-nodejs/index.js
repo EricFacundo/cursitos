6e4de67fee6ffb58170f4606fbdcc6066ea6be4d
Facundo Quiroz <facundoquiroz90@gmail.com>
2021-04-23 22:02:21 -0300

    borre todo perri

--- curso-nodejs/index.js
+++
@@ -1,9 +0,0 @@
-const math = require('./mates.js');
-
-console.log("Suma: ",math.sumar(2,3));
-
-console.log("Multiplicacion: ", math.multiplicar(3,3));
-
-console.log("Resta: ",math.restar(10,2));
-
-console.log("Division: ",math.dividir(10,0));
